16a2987dafce23e9b75b49a16e077444791d2f91
lixk <1749498702@qq.com>
2017-12-04 00:14:54 +0800

    add doc

--- convertutils.py
+++ convertutils.py
@@ -1,13 +1,21 @@
+# convert text/html to image or pdf util collections
+# author: lixk
+# email: 1749498702@qq.com
+
 import subprocess
 
 
 def render(url, filename, size='', zoom=''):
     """
     render html page to image or pdf file
+    
     :param url: html page addr
-    :param filename: generate file name
-    :param size: image size or pdf file format
-    :param zoom: 
+    :param filename: generate file name, suffix (png/jpg or pdf)
+    :param size: pdf paper format or image size\n
+        paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"\n
+        image (png/jpg output) examples: "1920px"-> entire page, window width 1920px;
+        "800px*600px" -> window clipped to 800px*600px
+    :param zoom: zoom factor
     :return: 
     """
     cmd = './lib/phantomjs.exe ./lib/rasterize.js {} {} {} {}'.format(url, filename, size, zoom)
